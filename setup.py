7903a875309c44be9a41bc77a57fffd0df8c69c3
Thijs de Zoete <thijsdezoete@gmail.com>
2013-07-11 16:12:36 +0200

    change long description

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
         name="noeval",
         version="1.0",
         description="No eval anymore",
+        long_description="This package removes eval from the global scope. Any call to eval will be pass'ed.",
         author="Thijs de Zoete",
         author_email="t@ht5ml.com",
         url="https://www.github.com/thijsdezoete/noevil",
